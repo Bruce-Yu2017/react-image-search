688ff031bf17994cd834a5e58972df93e1f98e7d
Bruce <ilywanzi@gmail.com>
2018-12-16 23:13:13 -0800

    Updates

---
+++ precache-manifest.572a9bba30dfcb191ad486628e3e4d0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f229191361f2902b70b",
+    "url": "/Bruce-Yu2017/react-image-search/static/js/runtime~main.4f229191.js"
+  },
+  {
+    "revision": "88e483e16cd7c06ec962",
+    "url": "/Bruce-Yu2017/react-image-search/static/js/main.88e483e1.chunk.js"
+  },
+  {
+    "revision": "14a92d67f8458cc67e39",
+    "url": "/Bruce-Yu2017/react-image-search/static/js/1.14a92d67.chunk.js"
+  },
+  {
+    "revision": "88e483e16cd7c06ec962",
+    "url": "/Bruce-Yu2017/react-image-search/static/css/main.fcee2e08.chunk.css"
+  },
+  {
+    "revision": "88f6028bac103043207f961b8808d069",
+    "url": "/Bruce-Yu2017/react-image-search/index.html"
+  }
+];
